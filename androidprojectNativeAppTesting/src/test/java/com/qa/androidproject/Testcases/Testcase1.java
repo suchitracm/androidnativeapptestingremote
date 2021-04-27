7529082af909b9ed847b703197ba196360d4bf65
suchitracm <suchitracm@yahoo.com>
2021-04-27 12:50:05 -0700

    Testcase1 and Testcase2 added

---
+++ androidprojectNativeAppTesting/src/test/java/com/qa/androidproject/Testcases/Testcase1.java
@@ -0,0 +1,8 @@
+package com.qa.androidproject.Testcases;
+
+public class Testcase1 {
+	public void setup()
+	{
+System.out.println("This is Testcase1");
+	}
+}
---
+++ androidprojectNativeAppTesting/src/test/java/com/qa/androidproject/Testcases/Testcase2.java
@@ -0,0 +1,9 @@
+package com.qa.androidproject.Testcases;
+
+public class Testcase2 {
+	public void setup()
+	{
+System.out.println("This is Testcase1");
+	}
+
+}
